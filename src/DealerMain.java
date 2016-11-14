9498eb49aee6bd6965d97a0b85c441aabe2f3577
mlp94 <mlepham94@gmail.com>
2016-11-14 14:48:59 -0600

    Update for structure of handleSequence

--- src/DealerMain.java
+++ src/DealerMain.java
@@ -6,54 +6,79 @@ public class DealerMain implements MAX_PLAYERS
 {
     public static final int value = (int)(6);
 	
-	// warning: poor programming standards within
-	// (class variables with void functions that modify class variables)
-	// Publisher class variables (bjDealer data publish)
-	public static DDSEntityManager mgrPub;
-	public static bjDealerTypeSupport typeSupportPub;
-	public static DataWriter writer;
-	public static bjDealerDataWriter typedWriter;
-	public static bjDealer pubInstance;
-	
-	// Subscriber class variables (bjPlayer data subscribe)
-	public static DDSEntityManager mgrSub;
-	public static bjPlayerTypeSupport typeSupportSub;
-	public static DataReader reader;
-	public static bjPlayerDataReader typedReader;
-	public static bjPlayerSeqHolder sequence;
-	public static SampleInfoSeqHolder sampleInfoSequence; // no idea what this does, leaving it in.
-	
-	
-	
 	public static void main(String[] args) {
-		mgrPub = new DDSEntityManager();
+		// Declare Publisher variables (bjDealer data publish)
+		DDSEntityManager mgrPub;
+		bjDealerTypeSupport typeSupportPub;
+		DataWriter writer;
+		bjDealerDataWriter typedWriter;
+		bjDealer pubInstance;
+		
+		// Declare Subscriber variables (bjPlayer data subscribe)
+		DDSEntityManager mgrSub;
+		bjPlayerTypeSupport typeSupportSub;
+		DataReader reader;
+		bjPlayerDataReader typedReader;
+		bjPlayerSeqHolder sequence;
+		SampleInfoSeqHolder sampleInfoSequence; // no idea what this does, leaving it in.
+		
+		// End PubSub declarations
+		
+		mgrPub = new DDSEntityManager(); // bjD type send
+		mgrSub = new DDSEntityManager(); // bjP type receive
 		String partitionName = "CasinoRoyale example";
 
 		// create Domain Participant, Type
 		mgrPub.createParticipant(partitionName);
 		typeSupportPub = new bjDealerTypeSupport();
-		mgrPub.registerType(typeSupportPub);
+		mgrPub.registerType(typeSupportPub); // send bjD
+		mgrSub.createParticipant(partitionName);
+		typeSupportSub = new bjPlayerTypeSupport();
+		mgrSub.registerType(typeSupportSub); // receive bjP
 
 		// create Publisher, DataWriter, Topic
-		mgrPub.createTopic("Casino_Royale"); // no spaces, should be same as other group's
+		mgrPub.createTopic("Casino_RoyaleDealerPub"); // no spaces, should be same as other group's
 		mgrPub.createPublisher();
 		mgrPub.createWriter();
-
-		// "Publish Events"
+		mgrSub.createTopic("Casino_RoyalePlayerPub");
+		mgrSub.createSubscriber();
+		mgrSub.createReader();
+		
+		// set up subscriber to read the p2p connection (on localhost) and sequences to receive a bjP
+		reader = mgrSub.getReader();
+		typedReader = bjPlayerDataReaderHelper.narrow(reader);
+		sequence = new bjPlayerSeqHolder();
+		sampleInfoSequence = new SampleInfoSeqHolder();
+		
+		// Prepare a bjDealer object for sending
 		writer = mgrPub.getWriter();
 		typedWriter = bjDealerDataWriterHelper.narrow(writer);
-		pubInstance = new bjDealer();
+		pubInstance = new bjDealer(); // TODO: Replace this with a bjDealer instance created by the Dealer class
 		
 		// Assign values
-		pubInstance.seqno = 1;
+		pubInstance.seqno = 05;
 		pubInstance.active_players = 6;
-		pubInstance.uuid = 1000123456;
+		pubInstance.uuid = 42;
+		pubInstance.action = CR.bjd_action.shuffling;
 		
 		typedWriter.register_instance(pubInstance); // finish assigning values, now save it to the instance for sending (or something like that)
 		
+		
+		
+		
+		System.out.println ("=== [Publisher] Sending bjDealer data");
+		
+		
+		
+		
+		
+		
+		
 		// Do the actual publishing
 		// run 10 times for testing
-		for(int i = 0; i<10; i++)
+		// for(int i = 0; i<10; i++)
+		
+		// run once
 		{
 			Timer.wait(50);
 			int status = typedWriter.write(pubInstance, HANDLE_NIL.value); // send the message instance
@@ -64,17 +89,90 @@ public class DealerMain implements MAX_PLAYERS
 		Timer.wait(1000);
 		
 		// clean up
-		cleanUpPublisher();
+		System.out.println();
+		cleanUpPublisher(mgrPub, typedWriter);
+		cleanUpSubscriber(mgrSub,typedReader);
+	}
+// end of main
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void handleSequenceDebug(bjPlayer obj)
+	{
+		System.out.println("\n=== [Subscriber] message received :\n");
+		
+		// seqno will be 0 for a player closing their connection
+		System.out.println("         seqno : " + obj.seqno); 
+		System.out.println("          uuid : " + obj.uuid);  
+	}
+	
+	// passes along the correct handling of information based on sequence number
+	public static void handleSequence(bjPlayer obj)
+	{
+		switch (obj.action.value())
+		{
+			case bjp_action._none: // dealer action when player is doing nothing
+				System.out.println("Player " + obj.uuid + " says none");
+				
+				
+				break;
+			case bjp_action._joining: // dealer action when player is joining
+				System.out.println("Player " + obj.uuid + " is joining");
+				
+				
+				break;
+			case bjp_action._exiting: // dealer action when player is exiting
+				System.out.println("Player " + obj.uuid + " is exiting");
+				
+				
+				break;
+			case bjp_action._wagering: // dealer action when player is wagering
+				System.out.println("Player " + obj.uuid + " is wagering");
+				
+				
+				break;
+			case bjp_action._requesting_a_card: // dealer action when player is requesting a card
+				System.out.println("Player " + obj.uuid + " is requesting a card");
+				
+				
+				break;
+			default:
+				break;
+		}
 	}
 	
 	
-	public static void cleanUpPublisher()
+	public static void cleanUpPublisher(DDSEntityManager mgrPub, bjDealerDataWriter typedWriter)
 	{
 		mgrPub.getPublisher().delete_datawriter(typedWriter);
 		mgrPub.deletePublisher();
 		mgrPub.deleteTopic();
 		mgrPub.deleteParticipant();
+		System.out.println ("Publisher connection closed.");
+	}
+	public static void cleanUpSubscriber(DDSEntityManager mgrSub, bjPlayerDataReader typedReader)
+	{
+		mgrSub.getSubscriber().delete_datareader(typedReader);
+		mgrSub.deleteSubscriber();
+		mgrSub.deleteTopic();
+		mgrSub.deleteParticipant();
+		System.out.println ("Subscriber connection closed.");
 	}
 	
 	
 }
+
+
+
