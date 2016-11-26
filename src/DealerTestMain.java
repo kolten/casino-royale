8cc81ea6fe86d249c34c3bdd497c1e576fd2942c
minhtea <minhtea@users.noreply.github.com>
2016-11-26 16:15:28 -0600

    Create DealerTestMain.java

---
+++ src/DealerTestMain.java
@@ -0,0 +1,58 @@
+import CR.bjDealer;
+import CR.bjPlayer;
+
+public class DealerTestMain
+{
+	public static void main(String[] args) {
+		DealerPub Pub = new DealerPub("CR example", "CR_bjDealer");
+		DealerSub Sub = new DealerSub("CR example", "CR_bjPlayer");
+		
+		
+		Timer PubTimer = new Timer();
+		
+		System.out.println("Writing Now");
+		PubTimer.start();
+		int i, k = 1;
+		while(PubTimer.getTimeMs() < 5000)
+		{
+			for(int j = 0; j < 6; j++)
+			{
+				bjDealer msg = new bjDealer();
+				msg.uuid = k;
+				msg.seqno = 2*k;
+				msg.active_players = 3*k;
+				i = Pub.write(msg);
+				if(i == 1)
+				{
+					Pub.printMsg(msg);
+					k++;
+				}
+			}
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException ie){}
+		}
+		System.out.printf("Wrote %d times\n", k);
+
+		i = 1;
+		System.out.println("Reading Now");
+		PubTimer.start();
+		while(PubTimer.getTimeMs() < 5000)
+		{
+			bjPlayer test = Sub.read();
+			if(test != null)
+			{
+				Sub.printMsg(test);
+				i++;
+			}
+		}
+		System.out.printf("Read %d times\n", i);
+
+		Pub.close();
+		Sub.close();
+	}
+}
+
+
