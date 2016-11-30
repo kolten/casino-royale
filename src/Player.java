5a44c569ff22bbcbfc76c7320e26c86650300acd
minhtea <minhtree124@gmail.com>
2016-11-29 20:28:06 -0600

    mind numbering

--- src/Player.java
+++ src/Player.java
@@ -207,7 +207,7 @@ public class Player {
 			wager = wager;
 		} else if(currentCredits > 5) {
 			//http://stackoverflow.com/questions/363681/generating-random-integers-in-a-specific-range
-			wager = rand.nextInt((MAX_BET - MIN_BET) + 1 ) + MIN_BET;
+			wager = 1;
 			//credits = credits - 1;
 			this.setCredits(credits);
 			action = bjp_action.wagering;
