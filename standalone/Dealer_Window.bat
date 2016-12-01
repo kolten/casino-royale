5f36b834b8e3953fe1c5ae3dd5f30e78619cfecc
amh5200 <amhdude1066@hotmail.com>
2016-11-30 21:19:50 -0600

    Didn't need one

--- standalone/Dealer_Window.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-color 2F
-title Dealer
-C:\HDE\x86_64.win64\release.bat
-chdir “C:\Users\Austin Hastings\casino-royale\standalone”
-pause
--- standalone/Player_Window.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-color %1
-title %3 %2
-C:\HDE\x86_64.win64\release.bat
-chdir “C:\Users\Austin Hastings\casino-royale\standalone”
-pause
