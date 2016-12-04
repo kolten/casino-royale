c535f999235e576a64105162e2ba5aee18aaf567
mlp94 <mlepham94@gmail.com>
2016-12-04 14:13:25 -0600

    Comments

--- src/UnitTestTimer.java
+++ src/UnitTestTimer.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 public class UnitTestTimer {
 
-    final int Wait_Time = 1000;
+    final int Wait_Time = 1000; // The timer should wait 1000 ms precisely
 
     @Test
     public void timerTest(){
         Timer timer = new Timer();
 
-        timer.start();
-        timer.wait(Wait_Time);
-        final long Test_time_elapsed = timer.getTimeMs();
+        timer.start(); // start timer (stopwatch)
+        timer.wait(Wait_Time); // puts thread to sleep for 1000 ms
+        final long Test_time_elapsed = timer.getTimeMs(); // get current time on stopwatch
 
         System.out.println("timerTest(): 1000 ms wait within 2 ms of error?");
 
