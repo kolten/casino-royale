9bae5d9e41fa3e666d98f7141dfc9549ee112983
minhtea <minhtree124@gmail.com>
2016-12-07 16:10:49 -0600

    Unit Test now useable for Testing Report

--- src/UnitTestBank.java
+++ src/UnitTestBank.java
@@ -6,12 +6,17 @@ public class UnitTestBank {
 
     final float Dealer_Start = 500f;
 	Bank bank = new Bank(Dealer_Start);
+	Dealer dealer = new Dealer();
 
     @Test
-    public void sampleTimerTest() {
-
-        System.out.println("Inside sampleBankTest()");    
+    public void testGetCredits() {
+        System.out.println("Inside testGetCredits()");    
         assertEquals(Dealer_Start, bank.getCredits(), 0.125f);
-           
+    }
+    
+    @Test
+    public void testDealerBankCredits() {
+        System.out.println("Inside testGetCredits()");    
+        assertEquals(Dealer_Start, dealer.getBank().getCredits(), 0.125f);
     }
 }
