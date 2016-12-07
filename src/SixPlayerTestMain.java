332d4406570eb59b40cff98feeb941fa64d3733d
minhtea <minhtree124@gmail.com>
2016-12-07 16:47:32 -0600

    It exists.

--- src/SixPlayerTestMain.java
+++ src/SixPlayerTestMain.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.Ignore;
 import static org.junit.Assert.*;
 
-public class SystemTestMain {
+public class SixPlayerTestMain {
 
 	static final int pauseBuffer = 600;
 	static final int shortBuffer = 20;	//Hard-coded read buffer.
@@ -13,7 +13,7 @@ public class SystemTestMain {
 
     @Test
     public void systemTest(){
-
+    	
         Thread pThread = new Thread () {
             public void run () {
                 PlayerMain pMain = new PlayerMain();
@@ -43,27 +43,54 @@ public class SystemTestMain {
                 dMain.run("Casino Royale", "bjDealer", "bjPlayer",shortBuffer,longBuffer);
             }
         };
-        // add 2 players
+        Thread pThread4 = new Thread () {
+            public void run () {
+                PlayerMain pMain = new PlayerMain();
+                pMain.player.setUuid(404);
+                pMain.run("Casino Royale", "bjPlayer", "bjDealer",pauseBuffer);
+            }
+        };
+        Thread pThread5 = new Thread () {
+            public void run () {
+                PlayerMain pMain = new PlayerMain();
+                pMain.player.setUuid(505);
+                pMain.run("Casino Royale", "bjPlayer", "bjDealer",pauseBuffer);
+            }
+        };
+        Thread pThread6 = new Thread () {
+            public void run () {
+                PlayerMain pMain = new PlayerMain();
+                pMain.player.setUuid(606);
+                pMain.run("Casino Royale", "bjPlayer", "bjDealer",pauseBuffer);
+            }
+        };
+        
+        // add 3 players
         pThread.start();
         pThread2.start();
-
+        pThread3.start();
+        
         // wait some time then start a dealer
         Timer.wait(5000);
         dThread.start();
 
-        // after some time, add a 3rd player
+        // after some time, add 2 more players
         Timer.wait(pauseBuffer*3);
-        pThread3.start();
-
+        pThread4.start();
+        pThread5.start();
+     // after some time, add another player
+        Timer.wait(pauseBuffer*3);
+        pThread6.start();
+        
         // wait for the last player and dealer to terminate
-        while(pThread.isAlive() || pThread2.isAlive() || pThread3.isAlive() || dThread.isAlive())
+        while(pThread.isAlive() || pThread2.isAlive() || pThread3.isAlive() || pThread4.isAlive() || pThread5.isAlive() || pThread6.isAlive() ||dThread.isAlive())
         {}
         assertTrue("Executables successfully ran",true);
     }
 
     public static void main(String[] args) {
         // test the SystemTestMain class
-        Result systemTest = JUnitCore.runClasses(SystemTestMain.class);
+        Result systemTest = JUnitCore.runClasses(SixPlayerTestMain.class);
             
         for (Failure failure : systemTest.getFailures()) {
             System.out.println(failure.toString());
